37c7072dc9c4cab3b8b2c5f15bed6ea85bdfe60a
ShadowGreg <shadow-greg@yandex.ru>
2022-10-13 16:01:15 +0300

    lesson_4

--- SnakeTool/Point.cs
+++ SnakeTool/Point.cs
@@ -1,16 +1,20 @@
 ﻿namespace SnakeTool;
 
+/*
+ инкапсуляция метод позволяющий объединить в себе
+ данные и методы работающие в классе и скрыть все детали реализации от пользователя
+ */
 public class Point
 {
     public int x;
     public int y;
     public char symbol;
 
-    public Point(int x, int y, char symbol)
+    public Point(int _x, int _y, char _symbol)
     {
-        this.x = x;
-        this.y = y;
-        this.symbol = symbol;
+        this.x = _x;
+        this.y = _y;
+        this.symbol = _symbol;
     }
 
     public void Draw()
