6bcd6c2e7ed10174230ea3404b467508b6127cc5
ShadowGreg <shadow-greg@yandex.ru>
2022-10-13 18:33:28 +0300

    lesson_6

--- SnakeTool/Program.cs
+++ SnakeTool/Program.cs
@@ -5,3 +5,16 @@ p1.Draw();
 var p2 = new Point(4, 5, '#');
 p2.Draw();
 Console.ReadKey();
+
+List<int> numList = new List<int>
+{
+    0,
+    1,
+    2
+};
+
+List<Point> pList = new List<Point>
+{
+    p1,
+    p2
+};
