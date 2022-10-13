f19bfe9430143c928de5fbc7b8390ef231e1e8a9
ShadowGreg <shadow-greg@yandex.ru>
2022-10-13 18:49:42 +0300

    lesson_7

---
+++ SnakeTool/HorizontalLine.cs
@@ -0,0 +1,24 @@
+﻿namespace SnakeTool;
+
+public class HorizontalLine
+{
+    private List<Point> plist;
+
+    public HorizontalLine(int xLeft, int xRight, int yHorizontal, char horizontalSymbol )
+    {
+        plist = new List<Point>();
+        for (int x = xLeft; x <= xRight; x++)
+        {
+            Point p = new Point(x, yHorizontal, horizontalSymbol);
+            plist.Add(p);
+        }
+    }
+
+    public void Drow()
+    {
+        foreach (Point VARIABLE in plist)
+        {
+            VARIABLE.Draw();
+        }
+    }
+}
--- SnakeTool/Program.cs
+++ SnakeTool/Program.cs
@@ -4,17 +4,8 @@ var p1 = new Point(1, 2, '*');
 p1.Draw();
 var p2 = new Point(4, 5, '#');
 p2.Draw();
+
+HorizontalLine line = new HorizontalLine(5,10, 8, '+');
+line.Drow();
+
 Console.ReadKey();
-
-List<int> numList = new List<int>
-{
-    0,
-    1,
-    2
-};
-
-List<Point> pList = new List<Point>
-{
-    p1,
-    p2
-};
