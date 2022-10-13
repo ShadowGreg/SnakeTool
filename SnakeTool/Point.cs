86e2f1ad43efe0f2c8c684f5e7d0a17105544891
ShadowGreg <shadow-greg@yandex.ru>
2022-10-13 15:47:38 +0300

    lesson_3

---
+++ SnakeTool/Point.cs
@@ -0,0 +1,21 @@
+﻿namespace SnakeTool;
+
+public class Point
+{
+    public int x;
+    public int y;
+    public char symbol;
+
+    public Point(int x, int y, char symbol)
+    {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+    }
+
+    public void Draw()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(symbol);
+    }
+}
--- SnakeTool/Program.cs
+++ SnakeTool/Program.cs
@@ -1,21 +1,7 @@
-﻿int x1 = 1;
-int y1 = 3;
-char symb1 = '*';
+﻿using SnakeTool;
 
-Draw(x1, y1, symb1);
-
-int x2 = 4;
-int y2 = 5;
-char symb2 = '*';
-
-Draw(x2, y2, symb2);
+var p1 = new Point(1, 2, '*');
+p1.Draw();
+var p2 = new Point(4, 5, '#');
+p2.Draw();
 Console.ReadKey();
-
-//процедурный подход повторяющие куски кода переносятся в отдельные функции
-//Минусы в том что есть переменные которые связаны и надо помнить что за что отвечает плс код длинный в одном файле
-
-static void Draw(int x, int y, char symbol)
-{
-    Console.SetCursorPosition(x, y);
-    Console.Write(symbol);
-}
