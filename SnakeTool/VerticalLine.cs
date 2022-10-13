551af4446e17a9527aa678b344b30d6d9a57b1a1
ShadowGreg <shadow-greg@yandex.ru>
2022-10-13 18:58:23 +0300

    add homework

---
+++ SnakeTool/VerticalLine.cs
@@ -0,0 +1,24 @@
+﻿namespace SnakeTool;
+
+public class VerticalLine
+{
+    private List<Point> plist;
+
+    public VerticalLine(int xVertical, int yHigher, int yBelow,  char verticalSymbol )
+    {
+        plist = new List<Point>();
+        for (int y = yBelow; y <= yHigher; y++)
+        {
+            Point p = new Point(xVertical, y, verticalSymbol);
+            plist.Add(p);
+        }
+    }
+
+    public void Drow()
+    {
+        foreach (Point VARIABLE in plist)
+        {
+            VARIABLE.Draw();
+        }
+    }
+}
